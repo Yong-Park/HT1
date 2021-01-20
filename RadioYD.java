5c43ae820ea19cf9e6f7072ad6ed967885160e5b
YongBum <59983323+Yong-Park@users.noreply.github.com>
2021-01-20 17:11:46 -0600

    Avanze 2
    Ya casi esta terminado solo requiere de unos cuantos ajustes mas

--- RadioYD.java
+++ RadioYD.java
@@ -2,6 +2,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class RadioYD implements RadioGeneral{
     //arrays
     private ArrayList<Integer> AM_Guardado=new ArrayList<Integer>();
@@ -62,12 +63,110 @@ public class RadioYD implements RadioGeneral{
     
     //para guardar una emisora
     public void asignar(){
-        
+        boolean ciclo=true;
+        int opcion_numero=0;
+
+        if(Tipo_canal.equals("AM")){
+            System.out.println("Emisora: " + AM);
+            System.out.println("Seleccione en donde lo piensa guardar");
+            for(int i=0;i<12;i++){
+                System.out.println((i+1) + ". " + AM_Guardado.get(i));
+            }
+            do{
+                try{
+                    String opcion = scanner.next();
+                    opcion_numero = Integer.parseInt(opcion);
+                    if(opcion_numero>=1 && opcion_numero<=12){
+                        ciclo=false;
+                    }else{
+                        System.out.println("Porfavor ingresa 1 o 12");
+                    }
+                }catch(Exception e){
+                    System.out.println("Ingersa solo datos numericos");
+                }
+            }while(ciclo);
+            
+            opcion_numero--;
+
+            AM_Guardado.set(opcion_numero, AM);
+        }else{
+            System.out.println("Emisora: " + FM);
+            System.out.println("Seleccione en donde lo piensa guardar");
+            for(int i=0;i<12;i++){
+                System.out.println((i+1) + ". " + FM_Guardado.get(i));
+            }
+            do{
+                try{
+                    String opcion = scanner.next();
+                    opcion_numero = Integer.parseInt(opcion);
+                    if(opcion_numero>=1 && opcion_numero<=12){
+                        ciclo=false;
+                    }else{
+                        System.out.println("Porfavor ingresa 1 o 12");
+                    }
+                }catch(Exception e){
+                    System.out.println("Ingersa solo datos numericos");
+                }
+            }while(ciclo);
+            
+            opcion_numero--;
+
+            FM_Guardado.set(opcion_numero, FM);
+        }
     }
     //para mostrar las emisoras guardadas y dirigirse a ellas
     public void emisoras(){
-        
+        boolean ciclo=true;
+        int opcion_numero=0;
+
+        if(Tipo_canal.equals("AM")){
+            System.out.println("Emisora: " + AM);
+            for(int i=0;i<12;i++){
+                if(AM_Guardado.get(i)!=0){
+                    System.out.println((i+1) +". " + AM_Guardado.get(i));
+                }
+            }
+            System.out.println("Seleccione una");
+            do{
+                try{
+                    String opcion = scanner.next();
+                    opcion_numero = Integer.parseInt(opcion);
+                    if(opcion_numero>=1 && opcion_numero<=12){
+                        ciclo=false;
+                    }else{
+                        System.out.println("Porfavor ingresa 1 o 12");
+                    }
+                }catch(Exception e){
+                    System.out.println("Ingersa solo datos numericos");
+                }
+            }while(ciclo);
+            opcion_numero--;
+            AM=AM_Guardado.get(opcion_numero);
+
+        }else{
+            System.out.println("Emisora: " + FM);
+            for(int i=0;i<12;i++){
+                if(FM_Guardado.get(i)!=0){
+                    System.out.println((i+1) +". " + FM_Guardado.get(i));
+                }
+            }
+            System.out.println("Seleccione una");
+            do{
+                try{
+                    String opcion = scanner.next();
+                    opcion_numero = Integer.parseInt(opcion);
+                    if(opcion_numero>=1 && opcion_numero<=12){
+                        ciclo=false;
+                    }else{
+                        System.out.println("Porfavor ingresa 1 o 12");
+                    }
+                }catch(Exception e){
+                    System.out.println("Ingersa solo datos numericos");
+                }
+            }while(ciclo);
+            opcion_numero--;
+            FM=FM_Guardado.get(opcion_numero);
+        }
     }
-
-
+    
 }
